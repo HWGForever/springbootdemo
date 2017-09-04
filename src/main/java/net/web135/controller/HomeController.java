80a924e38a2cec760e150864c536cd0ff573ba2e
刘涛 <tao.liu@minixiao.com>
2017-09-05 01:13:13 +0800

    homeController的添加，里边是一个示例，小刘博看一下~

--- src/main/java/net/web135/controller/HomeController.java
+++ src/main/java/net/web135/controller/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 
     @GetMapping("/hello")
     public String helloWorld(){
-        //TODO 刘博，这样使用注解的写法会非常省心，使用浏览器访问“http://localhost:8080/hello”就会返回“helloWorld! LiuBo”在页面，你可以继续写些其他的玩玩
+        //TODO 刘博，这样使用注解的写法会非常省心，使用浏览器访问“http://localhost:8080/hello”就会返回“helloWorld! LiuBo”在页面，你可以继续写些其他的玩
         return "helloWorld! LiuBo";
     }
 }
